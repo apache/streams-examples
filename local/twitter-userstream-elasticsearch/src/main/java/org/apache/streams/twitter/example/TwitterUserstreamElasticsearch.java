96129f444677002c470ce5159245c7c5aea05884
Steve Blackmon (@steveblackmon) <sblackmon@apache.org>
2015-03-31 10:48:12 -0500

    fixed delete verb def clean-up documentation confirmed working

--- local/twitter-userstream-elasticsearch/src/main/java/org/apache/streams/twitter/example/TwitterUserstreamElasticsearch.java
+++ local/twitter-userstream-elasticsearch/src/main/java/org/apache/streams/twitter/example/TwitterUserstreamElasticsearch.java
@@ -18,11 +18,15 @@
 
 package org.apache.streams.twitter.example;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.apache.streams.config.ComponentConfigurator;
 import org.apache.streams.config.StreamsConfigurator;
 import org.apache.streams.converter.ActivityConverterProcessor;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.core.StreamsProcessor;
 import org.apache.streams.elasticsearch.ElasticsearchPersistDeleter;
 import org.apache.streams.elasticsearch.ElasticsearchPersistWriter;
 import org.apache.streams.elasticsearch.ElasticsearchWriterConfiguration;
@@ -31,12 +35,16 @@ import org.apache.streams.filters.VerbDefinitionDropFilter;
 import org.apache.streams.filters.VerbDefinitionKeepFilter;
 import org.apache.streams.local.builders.LocalStreamBuilder;
 import org.apache.streams.core.StreamBuilder;
+import org.apache.streams.pojo.json.Activity;
 import org.apache.streams.twitter.TwitterStreamConfiguration;
 import org.apache.streams.twitter.provider.TwitterStreamProvider;
+import org.apache.streams.verbs.ObjectCombination;
 import org.apache.streams.verbs.VerbDefinition;
+import org.elasticsearch.common.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -48,7 +56,11 @@ public class TwitterUserstreamElasticsearch implements Runnable {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TwitterUserstreamElasticsearch.class);
 
-    private static VerbDefinition deleteVerbDefinition = new VerbDefinition().withValue("post");
+    /* this pattern will match any/only deletes */
+    private static VerbDefinition deleteVerbDefinition =
+            new VerbDefinition()
+            .withValue("delete")
+            .withObjects(Lists.newArrayList(new ObjectCombination()));
 
     TwitterUserstreamElasticsearchConfiguration config;
 
@@ -81,6 +93,7 @@ public class TwitterUserstreamElasticsearch implements Runnable {
         VerbDefinitionDropFilter noDeletesProcessor = new VerbDefinitionDropFilter(Sets.newHashSet(deleteVerbDefinition));
         ElasticsearchPersistWriter writer = new ElasticsearchPersistWriter(elasticsearchWriterConfiguration);
         VerbDefinitionKeepFilter deleteOnlyProcessor = new VerbDefinitionKeepFilter(Sets.newHashSet(deleteVerbDefinition));
+        SetDeleteIdProcessor setDeleteIdProcessor = new SetDeleteIdProcessor();
         ElasticsearchPersistDeleter deleter = new ElasticsearchPersistDeleter(elasticsearchWriterConfiguration);
 
         Map<String, Object> streamConfig = Maps.newHashMap();
@@ -92,10 +105,38 @@ public class TwitterUserstreamElasticsearch implements Runnable {
         builder.addStreamsProcessor("NoDeletesProcessor", noDeletesProcessor, 1, "converter");
         builder.addStreamsPersistWriter(ElasticsearchPersistWriter.STREAMS_ID, writer, 1, "NoDeletesProcessor");
         builder.addStreamsProcessor("DeleteOnlyProcessor", deleteOnlyProcessor, 1, "converter");
-        builder.addStreamsPersistWriter("deleter", deleter, 1, "DeleteOnlyProcessor");
+        builder.addStreamsProcessor("SetDeleteIdProcessor", setDeleteIdProcessor, 1, "DeleteOnlyProcessor");
+        builder.addStreamsPersistWriter("deleter", deleter, 1, "SetDeleteIdProcessor");
 
         builder.start();
 
+    }
+
+    protected class SetDeleteIdProcessor implements StreamsProcessor {
+
+        @Override
+        public List<StreamsDatum> process(StreamsDatum entry) {
+
+            Preconditions.checkArgument(entry.getDocument() instanceof Activity);
+            String id = entry.getId();
+            // replace delete with post in id
+            // ensure ElasticsearchPersistDeleter will remove original post if present
+            id = Strings.replace(id, "delete", "post");
+            entry.setId(id);
+
+            return Lists.newArrayList(entry);
+        }
+
+        @Override
+        public void prepare(Object configurationObject) {
+
+
+        }
+
+        @Override
+        public void cleanUp() {
+
+        }
     }
 
 }
